faa162001e10ec4bf013ca53e569d1da27f3fa24
Jayashree Balineni <115000642+jayashreebalinani@users.noreply.github.com>
2024-05-09 16:10:53 +0530

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,7 +1,6 @@
 var gross_data = sessionStorage.getItem("gross")
 var net_data = sessionStorage.getItem("net")
 var acc_data = sessionStorage.getItem("acc")
-
 document.getElementById("gross").innerText = gross_data
 document.getElementById("net").innerText = net_data
 document.getElementById("accuracy").innerText = acc_data + "%"
